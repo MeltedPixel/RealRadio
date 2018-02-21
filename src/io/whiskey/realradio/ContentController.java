6e7a3337ab366497e7f891d3e71a641c8afc9ce9
Michael Phillips <lastsound6@gmail.com>
2018-02-21 13:57:57 -0800

    [Core] - Started working on audio controller

--- src/io/whiskey/realradio/ContentController.java
+++ src/io/whiskey/realradio/ContentController.java
@@ -5,12 +5,23 @@
  */
 package io.whiskey.realradio;
 
+import static io.whiskey.realradio.Main.isDebugRunning;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
 
 /**
  *
@@ -22,14 +33,72 @@ public class ContentController implements Initializable {
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleButtonAction(ActionEvent event) throws MalformedURLException, IOException, JavaLayerException {
+
+        Thread t1 = new Thread("Audio Thread");
+
+        if (t1.isAlive()) {
+            System.out.format("%s is alive.%n", t1.getName());
+        } else {
+            System.out.format("%s is not alive.%n", t1.getName());
+            
+            InputStream input = null;
+            try {
+                input = new URL("http://d.liveatc.net/kpdx2").openStream();
+                Player player = new Player(input);
+                player.play();
+
+                Platform.runLater(() -> {
+
+                });
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(ContentController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException | JavaLayerException ex) {
+                Logger.getLogger(ContentController.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    input.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(ContentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } 
+        }
+        t1.start();
+        
+        
+        /*
+        Thread audioThread = new Thread() {
+            
+            @Override
+            public void run() {
+                InputStream input = null;
+                try {
+                    input = new URL("http://d.liveatc.net/kpdx2").openStream();
+                    Player player = new Player(input);
+                    player.play();
+                    
+                    Platform.runLater(() -> {
+                        
+                    });
+                } catch (MalformedURLException ex) {
+                    Logger.getLogger(ContentController.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException | JavaLayerException ex) {
+                    Logger.getLogger(ContentController.class.getName()).log(Level.SEVERE, null, ex);
+                } finally {
+                    try {
+                        input.close();
+                    } catch (IOException ex) {
+                        Logger.getLogger(ContentController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        };
+        audioThread.start();
+        */
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        
+    }   
 }
