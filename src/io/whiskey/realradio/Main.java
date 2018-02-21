4ea97a43ae5999252fd655b8ed168ac788eaa5a3
Michael Phillips <lastsound6@gmail.com>
2018-02-21 11:43:40 -0800

    [Core] - Added handling for GUI Frames

--- src/io/whiskey/realradio/Main.java
+++ src/io/whiskey/realradio/Main.java
@@ -7,24 +7,54 @@ package io.whiskey.realradio;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author MPHI14
  */
 public class Main extends Application {
+    public static final boolean isDebugRunning = true;
+    public static final String applicationName = "Real Radio";
+    public static Stage contentStage;
+    private double xOffset = 0;
+    private double yOffset = 0;
     
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage mainStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Content.fxml"));
         
-        Scene scene = new Scene(root);
+        Scene rootScene = new Scene(root);
         
-        stage.setScene(scene);
-        stage.show();
+        mainStage.initStyle(StageStyle.TRANSPARENT);
+        mainStage.setScene(rootScene);
+        mainStage.show();
+        mainStage.centerOnScreen();
+        mainStage.setTitle(applicationName);
+        mainStage.getIcons().add(new Image("io/whiskey/realradio/images/logoheadset.png"));
+        
+        contentStage = mainStage;
+        
+        rootScene.setOnMousePressed((MouseEvent event) -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        
+        rootScene.setOnMouseDragged((MouseEvent event) -> {
+            mainStage.setX(event.getScreenX() - xOffset);
+            mainStage.setY(event.getScreenY() - yOffset);
+        });
+        
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        mainStage.setX((primScreenBounds.getWidth() - mainStage.getWidth()) / 2); 
+        mainStage.setY((primScreenBounds.getHeight() - mainStage.getHeight()) / 2);
     }
 
     /**
